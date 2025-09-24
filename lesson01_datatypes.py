99a1b7d3676e45f4c66e7483f119a77356d2c955
Gabi <monteirogabby01@gmail.com>
2025-09-24 16:18:05 +0000

    creates datatypes lesson

---
+++ lesson01_datatypes.py
@@ -0,0 +1,43 @@
+# This is a comment
+#DATA TYPES
+
+# 1. STRINGS:
+print("Hello World")
+print(" ")
+print("Once upon a time there \nwas a very very very green frog \nthat radiated neon green")
+print("Name: \tBartholomew \nAge: \t6 \nGrade: \t1")
+
+# 2. INTEGERS
+print(" ")
+print(42)
+print(-23)
+print(5 + 5 - 11)
+print(5 * 10)
+print(10 / 2)
+
+# 3. FLOAT
+print(" ")
+print(4.0)
+print( 2.0 +4.5)
+print(2.0 + 4)
+
+# 4. BOOLEAN
+print(" ")
+print(True) #1
+print(False) #0
+print(True + True)
+print(True * False == 0)
+print(1 + 1 == 2)
+print(1 == 1.0)
+print(2 != 2) # != is not equals
+print(4.7 + 3.3 != 8.0)
+print("Hello" == "Hello")
+print("Hello" == "Good evening")
+print(5 + 5 == True * 10)
+
+#How do I know what data type I'm working with?
+print(" ")
+print(type("To see this message, wrap me in a print statement"))
+print(type(3.1415926535897932384626433832))
+print(type(1 == 1))
+print(type(3))
