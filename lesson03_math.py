55b04a460d577b0530d308f7a0409092866a98cb
Gabi <monteirogabby01@gmail.com>
2025-09-26 18:00:17 +0000

    basic math operators (PEMDAS!!)

---
+++ lesson03_math.py
@@ -0,0 +1,53 @@
+#KEY CONCEPTS: math operators +, -, *, /, //, %, **
+
+add = 7 + 2
+print("Sum: ", add)
+
+subtract = 7 - 2
+print("Difference: ", subtract)
+
+multiply = 7 * 2
+print("Product: ", multiply)
+
+divide = 7 / 2
+print("Quotient: ", divide)
+
+divide = 6 / 2
+print("Quotient: ", divide)
+
+float_divide = 6 / 2
+print("Float Division: ", float_divide)
+
+#One forward slash will always be a float
+
+integer_divide = 7 // 2
+print("Integer division: ", integer_divide)
+
+#Two forward slashes will round down from a float
+
+modulus = 7 % 2
+print("Modulus: ", modulus)
+
+exponent = 7 ** 2
+print("Exponent: ", exponent)
+
+print(2 + 2 == 10 - 6)
+
+print(2 + 2 != 10 - 6)
+
+#PEMDAS!!!!!!!!!!!!!!!!!!!
+ 
+result1 = (2 + 3) * 4
+print("Result 1: ", result1)
+
+result2 = 2 ** 3 * 4
+print("Result: ", result2) 
+
+result3 = 20 / 5 * 2
+print("Result 3: ", result3)
+
+result4 = 10 - 2 + 3
+print("Result 4: ", result4)
+
+result5 = 5 + 2 ** 3 * (4 - 1)
+print("Result 5: ", result5)
