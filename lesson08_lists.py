7d08ea5abfc40bf4d7c918398e7151e975e0b575
Gabi <monteirogabby01@gmail.com>
2025-10-16 18:23:41 +0000

    listststst

--- lesson08_lists.py
+++ lesson08_lists.py
@@ -4,10 +4,14 @@
 print()
 print("--- Lists in Python ---")
 
-animals = ["whale", "fox", "cat"]
+animals = ["whale", "fox", "Cat"]
 
 numbers = [2, 4, 6, 8, 10]
 mixed = ["kitty", 43, 3.14, False]
+
+empty_list = []
+empty_list.append("Microbat")
+print("Empty list:", empty_list)
 
 print("Animals:", animals)
 print("Numbers:", numbers)
@@ -43,5 +47,78 @@ last_animal = animals.pop()
 print("Removed animal:", last_animal)
 print("Remaining animals:", animals)
 
-animals.replace("fox", "tree frog")
-print(animals)
+animal_replace = animals.index("fox")
+print(animal_replace)
+animals[animal_replace] = "Microbat"
+print("Animals:", animals)
+
+
+print()
+print("--- Useful List Functions ---")
+print()
+
+nums = [4, 10, 5, 8, 3, 9, 2, 6, 1, 7]
+print("Original numbers:", nums)
+
+print("Length of the list:", len(nums))
+print("Minimum:", min(nums))
+print("Maximum:", max(nums))
+print("Sum:", sum(nums))
+
+nums.sort()
+print("Sorted:", nums)
+animals.sort()
+print("Animals sorted:", animals)
+
+nums.reverse()
+print("Numbers reversed:", nums)
+
+if "cat" in animals:
+    print("I like cat")
+else:
+    print("Where is cat")
+
+new_list = [4, 5, 6]
+copied_list = new_list.copy()
+copied_list.append(7)
+print(new_list)
+print(copied_list)
+
+print()
+print("--- Nested Lists/The Matrix ---")
+
+matrix = [ 
+    [1,2,3], 
+    [4,5,6], 
+    [7,8,9]
+    ]
+
+print(matrix[0][2])
+print(matrix[2][2])
+
+### **Challenge 1: Integer Swap**
+
+# Store this list in a variable: [ 1, 2, 3, 4, 5, 6 ] 
+# Ask the user to enter a new integer.
+# Replace the **third integer** in the original list with the user’s input, and then print the updated list.
+# *Hint: use indexing and assignment.*
+
+integers = [ 1, 2, 3, 4, 5, 6 ]
+new_num = int(input("Gimme a number "))
+integers[2] = new_num
+print("New list: ", integers)
+
+
+### **Challenge 2: Shopping List Manager**
+
+# Initialize an empty list called `shopping`.
+# Add three items of your choice using `.append()`.
+# Then insert one extra item at the second position and remove one item of your choice.
+# Finally, print the final shopping list.
+
+shopping = []
+shopping.append("Flour, baking powder, cocoa powder")
+shopping.insert(1, "milk")
+print(shopping)
+
+
