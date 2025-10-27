b7cca662166f6b1b74b78581ade580e928c06784
Gabi <monteirogabby01@gmail.com>
2025-10-27 17:03:55 +0000

    loops

--- lesson09_loops.py
+++ lesson09_loops.py
@@ -19,9 +19,69 @@ import time
 #     time.sleep(1.5)
 
 #     if bat == "Flying fox":
-#         print("Those don't look like foxes")
+#         print("Those don't even look like foxes")
 
-print("\nNow I have held all of the bats")
+# print("\nNow I have held all of the bats")
 
-for i in range(2, 11 , 2):
-    print("Even number:", i)
+print()
+
+# # range(start, stop, step)
+# print()
+# print("--- range() with start, stop, step ---")
+
+# for num in range(2, 11, 2):
+#     print("Even number:", num)
+# print("Loop ended. Evens appreciated.")
+
+print("--- Iterating over strings ---\n")
+
+fav_word = "Bingo"
+letter_list = []
+
+for letter in fav_word:
+    print(letter, end="")
+    letter_list.append(letter)
+    print(letter_list)
+
+print()
+
+
+# ---------------------------------------------------------
+# WHILE LOOPS
+# ---------------------------------------------------------
+# A while-loop repeats *while* a condition is true.
+# If you forget to change the condition, it loops forever.
+# And then your program becomes immortal. Avoid that.
+
+# += to add to a variable, -= to subtract to a variable, = to overright 
+import time
+count = 0
+
+while count < 5:
+    print(f"Looping. We are on loop number {count}.")
+    count += 1
+    time.sleep(0.5)
+
+print("We have escaped the loop!")
+
+user_input = ""
+
+print()
+while user_input != "exit":
+    user_input = input("Type 'exit' to escape: ")
+
+count = 60
+increment = 1
+
+while count > 0:
+    
+    count -= increment
+    increment += 1
+    
+    if count < 0:
+        break
+
+    
+
+    print(count)
+
